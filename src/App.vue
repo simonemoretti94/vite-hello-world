3704f2e115ec66e9ef5d79e9a2b66000682a0662
simonemoretti94 <simone.moretti.944@gmail.com>
2024-01-20 19:14:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+    <HelloWorld msg="Vite_hello_world!" />
+
+  </header>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 id="h1-hello">{{ msg }}</h1>
+</template>
+
+<style scoped>
+#h1-hello {
+  color: #0046dd;
+  text-shadow: 2px 3px black;
+}
+
+</style>
