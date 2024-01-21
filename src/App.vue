e081ff530c712856795108c9dac317a99c92f4b5
simonemoretti94 <simone.moretti.944@gmail.com>
2024-01-21 11:42:38 +0100

    2nd

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  export default {
+    data(){
+      return {
+        header_message: 'Hello_World!',
+      }
+    }
+  }
 </script>
 
 <template>
-  <header>
-
-    <HelloWorld msg="Vite_hello_world!" />
-
-  </header>
+    <h1>{{ header_message }}</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style>
 
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+</style>
