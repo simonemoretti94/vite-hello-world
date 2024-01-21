24143d05020c66994464335a65b2f29d26e7c81e
simonemoretti94 <simone.moretti.944@gmail.com>
2024-01-21 21:57:56 +0100

    3rd end

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <script>
-  export default {
-    data(){
-      return {
-        header_message: 'Hello_World!',
-      }
+export default {
+  data() {
+    return {
+      h1_message: 'Hello_World!',
     }
   }
+}
 </script>
 
 <template>
-    <h1>{{ header_message }}</h1>
+  <header>
+
+    <h1>{{ h1_message }}</h1>
+
+  </header>
 </template>
 
 <style>
+header {
+  min-height: 100px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+h1 {
+  color: blue;
+  font-style: italic;
+  text-align: center;
+  text-shadow: 2px 1px rgb(131, 131, 131);
+}
 </style>
